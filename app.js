cfef24967ee6a11af6d112cad0838d2261522273
aurel1313 <aurelienfabre439@gmail.com>
2021-08-26 11:03:10 +0200

    connexion

--- app.js
+++ app.js
@@ -3,24 +3,31 @@ const express =require('express');
 const app =express();
 const port =8080;
 const path = require('path');
+
 var ejs =require('ejs');
 const fs =require('fs')
 const covid = require('corona-info');
 const directory = path.join(__dirname,'src')
+
 app.use('/src',express.static(directory));
 app.set('views', path.join(__dirname, 'views')); 
 app.set('view engine', 'ejs');
 var bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({
     extended: true
   }));
-var sql =require('mysql');
+
+  var sql =require('mysql');
 var session = require('express-session')
 var data;
 var caseFrance,covidData,france;
+const ejsLint = require('ejs-lint');
 
-const validator=require('node-input-validator');
-const { Console } = require('console');
+const md5 = require('md5');
+const { check, validationResult } = require('express-validator');
+app.use(session({secret: 'ssshhhhh'}));
+var sess;
 app.get('/',(req,res)=>{
     //recuperer projets dans le dossier//
     console.log(__dirname)
@@ -33,15 +40,11 @@ app.get('/',(req,res)=>{
             })
         }
     })
-    var connexion =sql.createConnection({
-        host:'localhost',
-        user:'root',
-        password:'',
-        database:'portfoliov2'
-       
-    });
-    
-    res.render('index');
+   
+    sess=req.session;
+    sess.pseudo =req.body.pseudonyme
+   console.log(sess.pseudo)
+    res.render('index',{sess:sess});
 
 })
 
@@ -64,37 +67,64 @@ app.get('/inscription',(req,res)=>{
     res.render('inscription')
 })
 
-app.post('/inscription',function(req,res){
-    const v = new validator.Validator(req.body,{
-        pseudo:'required',
-        prenom:'required'
-    })
-      
-     
-     
-     
-    v.check().then((matched)=>{
-        if(!matched){
-             error = v.errors
-           
+app.use(express.json());
+app.post('/inscription',[
+check('prenom').isLength({min:5}),
+check('email').isEmail(),
+//check('password').isLength({min:5}).withMessage('le password doit etre plus securisé')
+   
+],
+(req,res)=>{
+    const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() })
+  }
+    let prenom =req.body.prenom;
+    let nom =req.body.nom;
+    let email = req.body.email;
+    let password =req.body.mdp;
+    let pseudonyme=req.body.pseudonyme
+    let hash =md5(password);
+    let date=req.body.date;
+    
+      var connexion =sql.createConnection({
+                host:'falbala.futaie.org',
+                user:'fabrea',
+                password:'quahroh4pait',
+                database:'fabrea',
+                port:'3306'
+               
+            });
+            connexion.connect(function(err){
+                if(err) throw err;
+                console.log('connected')
+                var insert ='INSERT INTO user(prenom,nom,pseudo,mdp,date_naiss) VALUES ?';
+                var values=[
+                    [prenom,nom,pseudonyme,hash,date]
+                ];
+                connexion.query(insert,[values],function(err,result){
+                    if(err) throw err;
+                    console.log(result.affectedRows)
+                })
+               
+            })
+        sess=req.session;
+        sess.pseudo =req.body.pseudonyme
+        if(sess.pseudo){
+            res.render('index',{sess:sess})
         }
-         
-        
-        /*var connexion =sql.createConnection({
-            host:'localhost',
-            user:'root',
-            password:'',
-            database:'portfoliov2'
-           
-        });
-        connexion.connect(function(err){
-            if(err) throw err;
-            
-        })*/
-       
-    })
-    res.render('inscription')
+    
+   
+    
 })
+app.get('/connexion',(req,res)=>{
+    res.render('connexion')
+})
+app.get('/deconnexion',(req,res)=>{
+    res.render('deconnexion',{sess:sess})
+})
+    
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
   })
